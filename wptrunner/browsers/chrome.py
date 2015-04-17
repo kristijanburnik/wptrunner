5a104e4ce2d9342903a11786958ee66701ca60c8
James Graham <james@hoppipolla.co.uk>
2015-04-17 17:11:18 +0100

    Add initial support for running tests on sauce labs

--- wptrunner/browsers/chrome.py
+++ wptrunner/browsers/chrome.py
@@ -7,19 +7,20 @@ from .webdriver import ChromedriverLocalServer
 from ..executors import executor_kwargs as base_executor_kwargs
 from ..executors.executorselenium import (SeleniumTestharnessExecutor,
                                           SeleniumRefTestExecutor)
-
+from ..environment import LocalServerEnvironment
 
 __wptrunner__ = {"product": "chrome",
                  "check_args": "check_args",
                  "browser": "ChromeBrowser",
                  "executor": {"testharness": "SeleniumTestharnessExecutor",
                               "reftest": "SeleniumRefTestExecutor"},
+                 "env": "LocalServerEnvironment",
                  "browser_kwargs": "browser_kwargs",
                  "executor_kwargs": "executor_kwargs",
                  "env_options": "env_options"}
 
 
-def check_args(**kwargs):
+def check_args(kwargs):
     require_arg(kwargs, "binary")
 
 
